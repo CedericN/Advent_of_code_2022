6d2f4cffe8417e3e24594b57a4060c915b795348
Cederic Work <nijssen.cederic@gmail.com>
2022-12-05 14:48:08 +0200

    Template day's

---
+++ 0_Template/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string day = DAY;
+    day = day.at(0) == '0' ? day.substr(1) : day;
+    cout << "One moment please i'm solving day " << day << endl;
+    cout << (day.length() < 2 ? "..................................." : "....................................") << endl << endl;
+    return 0;
+}
