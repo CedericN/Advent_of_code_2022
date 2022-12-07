3447617a45593c01e50dfe6226c34b06577eb16c
Cederic Work <nijssen.cederic@gmail.com>
2022-12-07 11:17:01 +0200

    update Template

--- 0_Template/main.cpp
+++ 0_Template/main.cpp
@@ -7,5 +7,8 @@ int main() {
     day = day.at(0) == '0' ? day.substr(1) : day;
     cout << "One moment please i'm solving day " << day << endl;
     cout << (day.length() < 2 ? "..................................." : "....................................") << endl << endl;
+
+
+
     return 0;
 }
